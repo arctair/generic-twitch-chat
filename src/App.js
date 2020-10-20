32949465e0b07d5fcc792cccced864fdbf5c8062
arctair <jtyler_@outlook.com>
2020-10-20 18:05:51 -0500

    handle connection error

--- src/App.js
+++ src/App.js
@@ -2,27 +2,38 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 
 function Chat({ messages }) {
-  return (
-    <>
-      {messages.map((message, index) => (
-        <div key={index}>{message}</div>
-      ))}
-    </>
+  return messages.length ? (
+    <ChatMessages messages={messages} />
+  ) : (
+    <div>Connecting...</div>
   )
+}
+
+function ChatMessages({ messages }) {
+  return messages.map((message, index) => <div key={index}>{message}</div>)
 }
 
 function useMessages() {
   const [messages, setMessages] = useState([])
   useEffect(() => {
-    const webSocket = new WebSocket('wss://irc-ws.chat.twitch.tv:443', 'irc')
+    const webSocket = new WebSocket(
+      'wss://irc-ws.chat.twitch.tv:443',
+      'irc',
+    )
     webSocket.addEventListener('message', ({ data }) => {
       console.log('new message', data, typeof data)
-      setMessages(messages => messages.concat('>>>' + data))
+      setMessages((messages) => messages.concat('>>>' + data))
     })
     webSocket.addEventListener('open', () => {
       webSocket.send('PASS foobar')
       webSocket.send('NICK justinfan123')
       webSocket.send(`JOIN #krispykitty`)
+    })
+    webSocket.addEventListener('error', (event) => {
+      setMessages((messages) =>
+        messages.concat('Could not connect to chat'),
+      )
+      console.error(event)
     })
     return () => webSocket.close()
   }, [])
@@ -30,8 +41,7 @@ function useMessages() {
 }
 
 function App() {
-  const messages = useMessages()
-  return <Chat messages={messages} />
+  return <Chat messages={useMessages()} />
 }
 
 export default App

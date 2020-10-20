b516abc8a8b9d5f4bae0e2a244ccab2301ce80f3
arctair <jtyler_@outlook.com>
2020-10-20 17:52:26 -0500

    get messages from twitch

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 function Chat({ messages }) {
   return (
     <>
-      {messages.map(({ message, user }, index) => (
-        <div key={index}>
-          {user}: {message}
-        </div>
+      {messages.map((message, index) => (
+        <div key={index}>{message}</div>
       ))}
     </>
   )
 }
 
+function useMessages() {
+  const [messages, setMessages] = useState([])
+  useEffect(() => {
+    const webSocket = new WebSocket('wss://irc-ws.chat.twitch.tv:443', 'irc')
+    webSocket.addEventListener('message', ({ data }) => {
+      console.log('new message', data, typeof data)
+      setMessages(messages => messages.concat('>>>' + data))
+    })
+    webSocket.addEventListener('open', () => {
+      webSocket.send('PASS foobar')
+      webSocket.send('NICK justinfan123')
+      webSocket.send(`JOIN #krispykitty`)
+    })
+    return () => webSocket.close()
+  }, [])
+  return messages
+}
+
 function App() {
-  const messages = [
-    {
-      user: 'jsonwebdev',
-      message: 'i love trailing commas',
-    },
-    {
-      user: 'ozown',
-      message: 'i need html',
-    },
-  ]
+  const messages = useMessages()
   return <Chat messages={messages} />
 }
 
